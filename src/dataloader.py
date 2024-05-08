cdc94f6194cff0154cd81bda6049980684579d62
Alan Yang <hua.void@gmail.com>
2024-05-07 23:37:43 -0400

    feat: update goals into tree-structured in dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -6,7 +6,7 @@ from metamathpy.database import parse
 from metamathpy.proof import verify_proof
 import matplotlib.pyplot as pt
 
-from utils import extract_proof_labels
+from utils import extract_proof_labels, build_tree, traverse_tree, max_value_len, TreeNode
 
 
 db = parse("set.mm")
@@ -24,34 +24,41 @@ for r, rule in enumerate(db.rules.values()):
 
     goal = rule.consequent.tokens
     goal_tokens |= set(goal)
+    goal = build_tree(goal)
 
     proof_root, _ = verify_proof(db, rule)
+    if len(proof_root.dependencies) == 0:
+        print(f"no proof for {rule.consequent.label}")
+        continue
     labels = extract_proof_labels(proof_root)
+    print(labels)
     label_tokens |= set(labels)
-
     examples.append((goal, labels))
 
 print("raw examples:")
 print("example 0:", examples[0])
-print("example -1:", examples[-1])
+print("example -10:", examples[-10])
 
 # replace tokens with integer indexes
 idx2goal = list(goal_tokens)
 idx2label = list(label_tokens)
 
-goal2idx = {token: t for t, token in enumerate(idx2goal)}
+goal2idx = {token: t + 1 for t, token in enumerate(idx2goal)}
 label2idx = {token: t for t, token in enumerate(idx2label)}
 
+max_goal_len = max([max_value_len(goal) for goal, _ in examples])
+
 examples = [
-    ([goal2idx[tok] for tok in goal], [label2idx[tok] for tok in labels])
+    (traverse_tree(goal, lambda x: goal2idx[x]), [label2idx[tok] for tok in labels])
     for (goal, labels) in examples
 ]
 
 if __name__ == "__main__":
     print("indexed examples:")
     print("example 0:", examples[0])
-    print("example -1:", examples[-1])
+    print("example -10:", examples[-10])
     print(f"{len(goal_tokens)} goal tokens, {len(label_tokens)} label tokens")
+    print(f"max goal tree node size: {max_goal_len}")
 
     with open("dataset.pkl", "wb") as f:
         pickle.dump(examples, f)
@@ -59,9 +66,10 @@ if __name__ == "__main__":
         pickle.dump(idx2label, f)
         pickle.dump(len(goal_tokens), f)
         pickle.dump(len(label_tokens), f)
+        pickle.dump(max_goal_len, f)
 
-    pt.hist([len(proof) for (goal, proof) in examples], bins=100)
-    pt.xlabel("Proof length")
-    pt.ylabel("Frequency")
-    pt.yscale("log")
-    pt.savefig("proof_lengths.png")
+    # pt.hist([len(proof) for (goal, proof) in examples], bins=100)
+    # pt.xlabel("Proof length")
+    # pt.ylabel("Frequency")
+    # pt.yscale("log")
+    # pt.savefig("proof_lengths.png")
